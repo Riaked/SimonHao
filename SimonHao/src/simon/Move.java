a2e6e8ad82613667939b5910494f53d0bcd5c66d
Jenniber Franco <jfranco7156@bths.edu>
2017-01-08 15:29:26 -0500

    New Classes

---
+++ SimonHao/src/simon/Move.java
@@ -0,0 +1,11 @@
+package simon;
+
+public class Move implements MoveInterfaceHao {
+
+	@Override
+	public ButtonInterfaceHao getButton() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
