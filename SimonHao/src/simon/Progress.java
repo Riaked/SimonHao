bc90d6371fb8aa982aec0f3613638b1ba5a8a88c
Jenniber Franco <jfranco7156@bths.edu>
2017-01-08 14:35:34 -0500

    New Classes

---
+++ SimonHao/src/simon/Progress.java
@@ -0,0 +1,25 @@
+package simon;
+
+import java.awt.Component;
+
+public class Progress extends Component implements ProgressInterfaceHao {
+
+	@Override
+	public void gameOver() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setRound(int roundNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setSequenceLength(int size) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
